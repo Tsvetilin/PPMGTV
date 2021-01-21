92c3338137688cbbe1929f54a95243a2c3126bc2
Цветилин Цветилов <cvetilov6@abv.bg>
2021-01-21 17:02:01 +0200

    Fix cookie consent, translate contact page models errors

--- src/Web/Models/Contact/ContactInputModel.cs
+++ src/Web/Models/Contact/ContactInputModel.cs
@@ -6,23 +6,25 @@ namespace Web.Models.Contact
 {
     public class ContactInputModel 
     {
-        [Required]
-        [MaxLength(100)]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Моля въведете тема на съобщението")]
+        [StringLength(100, ErrorMessage = "Съобщението трябва да е поне {2} символа и не повече от {1} символа.", MinimumLength = 3)]
         [DisplayName("Относно")]
         public string About { get; set; }
 
-        [Required]
         [DisplayName("Описание")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Моля въведете съдържание на съобщението")]
+        [StringLength(100000, ErrorMessage = "Съобщението трябва да е поне {2} символа.", MinimumLength = 20)]
         public string Description { get; set; }
 
-        [Required]
-        [MaxLength(50)]
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Моля въведете вашето име")]
+        [StringLength(50, ErrorMessage = "Дължината на името трябва да е поне {2} символа и не повече от {1} символа.", MinimumLength = 2)]
         [DisplayName("Подател (име)")]
         public string SenderName { get; set; }
 
-        [Required]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Моля въведете вашето име")]
         [MaxLength(50)]
-        [EmailAddress]
+        [EmailAddress(ErrorMessage ="Невалиден имейл адрес.")]
         [DisplayName("Имейл за връзка")]
         public string SenderEmail { get; set; }
 
